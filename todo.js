f74fd1a53f43ca5b2d8eacbb2c1e5d4abc515437
kushwah-Anjali <Kushwah-Anjali>
2026-03-19 08:48:07 +0530

    understand till the delete functionality

--- todo.js
+++ todo.js
@@ -1,8 +1,28 @@
 let input = document.querySelector("input");
 let addbtn = document.querySelector("button");
-let taskList=document.querySelector("#task-list");
+let taskList = document.querySelector("#task-list");
 addbtn.addEventListener("click", () => {
-  console.log(input.value);
-  
+  if (input.value === "") return;
+
+  let list = document.createElement("li");
+
+  let deletebtn = document.createElement("button");
+  let updatebtn = document.createElement("button");
+
+  list.textContent = input.value;
+
+  deletebtn.textContent = "delete";
+  updatebtn.textContent = "update";
+
+  list.appendChild(deletebtn);
+  list.appendChild(updatebtn);
+
+  taskList.appendChild(list);
+  deletebtn.addEventListener("click", () => {
+    list.remove();
+  });
+  updatebtn.addEventListener("click", () => {
+    
+  });
   input.value = "";
 });
