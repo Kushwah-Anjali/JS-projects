9df45dc69a6ab6a776bb1ed88cd2a2de825982a8
kushwah-Anjali <Kushwah-Anjali>
2026-03-20 11:40:24 +0530

    add task editing with state management and conditional add/update logic

--- todo.js
+++ todo.js
@@ -4,25 +4,33 @@ let addTask = document.querySelector("#add");
 let currentTask = null;
 
 addTask.addEventListener("click", () => {
-  if (input.value === " ") return;
-  let list = document.createElement("li");
-  let updateBtn = document.createElement("button");
-  let deleteBtn = document.createElement("button");
-  let task = document.createElement("span");
-  task.textContent = input.value;
+  if (currentTask !== null) {
+    currentTask.textContent = input.value;
+    input.value = "";
+    addTask.textContent = "Add Task";
+    currentTask = null;
+  } else {
+    if (input.value.trim() === "") return;
+    let list = document.createElement("li");
+    let updateBtn = document.createElement("button");
+    let deleteBtn = document.createElement("button");
+    let task = document.createElement("span");
+    task.textContent = input.value;
+    updateBtn.textContent = "update";
+    deleteBtn.textContent = "delete";
+    list.appendChild(task);
+    list.appendChild(updateBtn);
+    list.appendChild(deleteBtn);
+    taskList.appendChild(list);
+    deleteBtn.addEventListener("click", () => {
+      list.remove();
+    });
+    updateBtn.addEventListener("click", () => {
+      currentTask = task;
+      input.value = task.textContent;
+      addTask.textContent = "update";
+    });
 
-  updateBtn.textContent = "update";
-  deleteBtn.textContent = "delete";
-  list.appendChild(task);
-  list.appendChild(updateBtn);
-  list.appendChild(deleteBtn);
-  taskList.appendChild(list);
-  deleteBtn.addEventListener("click", () => {
-    list.remove();
-  });
-  updateBtn.addEventListener("click", () => {
-    currentTask = task.textContent;
-    input.value =currentTask;
-  });
-  input.value = "";
+    input.value = "";
+  }
 });
