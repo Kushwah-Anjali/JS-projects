32166f5549b0695a4645d3bf1c88c9a52810bd57
kushwah-Anjali <Kushwah-Anjali>
2026-03-22 07:50:16 +0530

    Basic understanding of dom . now moving to event delegation

--- todo.js
+++ todo.js
@@ -3,41 +3,41 @@ let addBtn = document.querySelector("#add");
 let taskList = document.querySelector("#task-list");
 let currentTask = null;
 addBtn.addEventListener("click", () => {
-  createTask();
-});
-function createTask() {
+  if (input.value.trim() === "") return;
   if (currentTask !== null) {
-    if (input.value.trim() === "") return;
     updateTask();
   } else {
-    if (input.value.trim() === "") return;
-    let list = document.createElement("li");
-    let task = document.createElement("span");
-    let deletBtn = document.createElement("button");
-    let updateBtn = document.createElement("button");
-    task.textContent = input.value;
-    deletBtn.textContent = "Remove";
-    updateBtn.textContent = "Update";
-    list.appendChild(task);
-    list.appendChild(deletBtn);
-    list.appendChild(updateBtn);
-    taskList.appendChild(list);
-
-    deletBtn.addEventListener("click", () => {
-      deleteTask(list);
-    });
-    updateBtn.addEventListener("click", () => {
-      currentTask = task;
-      input.value = task.textContent;
-      addBtn.textContent = "update";
-    });
+    createTask();
   }
+});
+function createTask() {
+  let list = document.createElement("li");
+  let task = document.createElement("span");
+  let deletBtn = document.createElement("button");
+  let updateBtn = document.createElement("button");
+  task.textContent = input.value;
+  deletBtn.textContent = "Remove";
+  updateBtn.textContent = "Update";
+  list.appendChild(task);
+  list.appendChild(deletBtn);
+  list.appendChild(updateBtn);
+  taskList.appendChild(list);
+
+  deletBtn.addEventListener("click", () => {
+    deleteTask(list);
+  });
+  updateBtn.addEventListener("click", () => {
+    currentTask = task;
+    input.value = task.textContent;
+    addBtn.textContent = "update";
+  });
   input.value = "";
 }
 function updateTask() {
   currentTask.textContent = input.value;
   addBtn.textContent = "Add Task";
   currentTask = null;
+    input.value = "";
 }
 
 function deleteTask(list) {
