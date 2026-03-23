91ff2c00fa570925bf1c19629a5321ffebeaca17
kushwah-Anjali <Kushwah-Anjali>
2026-03-23 05:57:32 +0530

    Use bootsrap framework to give todo a basic style or ui

--- todo.js
+++ todo.js
@@ -16,31 +16,51 @@ function createTask() {
   let deletBtn = document.createElement("button");
   let updateBtn = document.createElement("button");
   task.textContent = input.value;
+  list.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
   deletBtn.textContent = "Remove";
   updateBtn.textContent = "Update";
+  deletBtn.classList.add("btn", "btn-outline-danger", "btn-sm", "delete");
+  updateBtn.classList.add("btn", "btn-outline-info", "btn-sm", "update");
+  deletBtn.innerHTML = `<i class="bi bi-trash"></i>`;
+  updateBtn.innerHTML = `<i class="bi bi-pencil"></i>`;
+  let btnGroup = document.createElement("div");
+  btnGroup.classList.add("btn-group");
+  btnGroup.appendChild(updateBtn);
+  btnGroup.appendChild(deletBtn);
   list.appendChild(task);
-  list.appendChild(deletBtn);
-  list.appendChild(updateBtn);
+  list.appendChild(btnGroup);
   taskList.appendChild(list);
-
-  deletBtn.addEventListener("click", () => {
-    deleteTask(list);
-  });
-  updateBtn.addEventListener("click", () => {
-    currentTask = task;
-    input.value = task.textContent;
-    addBtn.textContent = "update";
-  });
   input.value = "";
 }
+
 function updateTask() {
   currentTask.textContent = input.value;
-  addBtn.textContent = "Add Task";
+  addBtn.innerHTML = `<i class="bi bi-plus-lg"></i>`;
+  addBtn.classList.remove("btn-warning");
+  addBtn.classList.add("btn-primary");
   currentTask = null;
-    input.value = "";
+  input.value = "";
 }
 
 function deleteTask(list) {
   list.remove();
   input.value = "";
 }
+taskList.addEventListener("click", (e) => {
+  if (e.target.closest(".delete")) {
+    e.target.closest("li").remove();
+  }
+  if (e.target.closest(".update")) {
+    let task = e.target.closest("li").querySelector("span");
+    currentTask = task;
+    input.value = task.textContent;
+    addBtn.innerHTML = `<i class="bi bi-check-lg"></i>`;
+    addBtn.classList.remove("btn-primary");
+    addBtn.classList.add("btn-warning");
+  }
+});
